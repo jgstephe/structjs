5b6e8d6c75f96e6d1e1961a2c4305c4b91ee320c
rkusa <m@rkusa.st>
2013-08-18 14:02:02 +0200

    Implement Number Writing

--- test/number.js
+++ test/number.js
@@ -1,9 +1,10 @@
 var should = require('should')
   , fs     = require('fs')
+  , crypto = require('crypto')
   , Struct = require('../lib/struct')
   , utils  = require('./utils')
 
-suite('Number')
+suite('Number - Read')
 
 var Test = new Struct({
   Int8:    Struct.Int8,
@@ -19,12 +20,23 @@ var Test = new Struct({
 var t = new Test
 var buffer = fs.readFileSync(__dirname + '/data/number.test')
 t.unpack(new DataView(utils.toArrayBuffer(buffer)))
-  
+
 test('Int8',    function() { t.Int8.should.eql(0x42)                  })
 test('UInt8',   function() { t.UInt8.should.eql(0x42)                 })
 test('Int16',   function() { t.Int16.should.eql(0xacd)                })
 test('UInt16',  function() { t.UInt16.should.eql(0xdead)              })
 test('Int32',   function() { t.Int32.should.eql(0xacdface)            })
 test('UInt32',  function() { t.UInt32.should.eql(0xfeedface)          })
-test.skip('Float32', function() { t.Float32.should.eql(0xcafebabe)         })
+test('Float32', function() { t.Float32.should.eql(0xdead0000)         })
 test('Float64', function() { t.Float64.should.eql(0xdeadbeefcafebabe) })
+
+suite('Number - Write')
+
+var packed = utils.toBuffer(t.pack())
+
+test('checksum should be equal', function() {
+  crypto.createHash('md5').update(buffer).digest('base64')
+    .should.eql(
+      crypto.createHash('md5').update(packed).digest('base64')
+    )
+})
--- test/utils.js
+++ test/utils.js
@@ -1,8 +1,17 @@
 exports.toArrayBuffer = function(buffer) {
-  var ab = new ArrayBuffer(buffer.length);
-  var view = new Uint8Array(ab);
+  var ab = new ArrayBuffer(buffer.length)
+  var view = new Uint8Array(ab)
   for (var i = 0; i < buffer.length; ++i) {
-      view[i] = buffer[i];
+      view[i] = buffer[i]
   }
-  return ab;
+  return ab
+}
+
+exports.toBuffer = function(ab) {
+  var buffer = new Buffer(ab.byteLength)
+  var view = new Uint8Array(ab)
+  for (var i = 0; i < buffer.length; ++i) {
+      buffer[i] = view[i]
+  }
+  return buffer
 }
